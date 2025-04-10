dff5d8938e0198d22c7959ca6d72dcb72dfa2076
Saran Raj <saranrajalakshmi2296@gmail.com>
2025-04-10 13:24:17 +0530

    Second commit

---
+++ Leetcode/Easy/1207. Unique Number of Occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);    
+        }
+        Set<Integer> set=new HashSet<>();
+        for(int key:map.keySet()){
+            if(set.contains(map.get(key))) return false;
+            set.add(map.get(key));
+        }
+        
+        return true;
+        
+    }
+}
