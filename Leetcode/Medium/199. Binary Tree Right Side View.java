651ddcb0ff139b95d710b55d78015f280cea2d9c
Sa-ran-raj <145576913+Sa-ran-raj@users.noreply.github.com>
2025-09-05 15:33:42 +0530

    Create 199. Binary Tree Right Side View.java

---
+++ Leetcode/Medium/199. Binary Tree Right Side View.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> ans;
+    public void dfs(TreeNode root, int depth){
+        if(root==null) return;
+        if(depth==ans.size()) ans.add(root.val);
+
+        dfs(root.right,depth+1);
+        dfs(root.left,depth+1);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        ans=new ArrayList<>();
+        dfs(root,0);
+        return ans;
+    }
+}
