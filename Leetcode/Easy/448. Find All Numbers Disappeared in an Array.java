f507991d5ddcb9340a4ac5c7e2a7ac9e6e6af080
Saranraj A R <145576913+SaranrajAR@users.noreply.github.com>
2025-10-26 20:40:27 +0530

    Create 448. Find All Numbers Disappeared in an Array.java

---
+++ Leetcode/Easy/448. Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n=nums.length;
+        int[] arr=new int[n+1];
+        for(int i=0;i<n;i++){
+            arr[nums[i]]=1;
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(arr[i]==0) ans.add(i);
+        }
+        return ans;
+    }
+}
