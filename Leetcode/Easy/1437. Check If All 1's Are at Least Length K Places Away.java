67d4f6fa3af4560a59d0bb862513c7b9d3d72f56
Saranraj A R <145576913+SaranrajAR@users.noreply.github.com>
2025-11-17 18:04:08 +0530

    Add solution for checking distance of 1's in array

---
+++ Leetcode/Easy/1437. Check If All 1's Are at Least Length K Places Away.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int prevIdx=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                if(prevIdx!=-1 && i-prevIdx<=k){
+                    return false;
+                }
+                prevIdx=i;
+            }
+        }
+        return true;
+    }
+}
