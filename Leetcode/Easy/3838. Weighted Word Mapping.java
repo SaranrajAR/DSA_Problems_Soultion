07760e2b13668d079873c0d58c831219fc4dffd4
Saranraj A R <145576913+SaranrajAR@users.noreply.github.com>
2026-02-15 09:19:26 +0530

    Add solution for weighted word mapping problem

---
+++ Leetcode/Easy/3838. Weighted Word Mapping.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder sb=new StringBuilder();
+        for(String word:words){
+            int sum=0;
+            for(char ch:word.toCharArray()) sum+=weights[ch-'a'];
+            int idx=25-(sum)%26;
+            sb.append((char)('a'+idx));
+        }
+        return sb.toString();
+    }
+}
